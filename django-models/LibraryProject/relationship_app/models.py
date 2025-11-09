9a7656d2081b4cde312a1d915e02fbcb9bf338b1
Emmanuel047 <emwenwa007@gmail.com>
2025-11-09 16:43:31 +0300

    self.title

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,14 +5,24 @@ from django.forms import CharField
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 class Library(models.Model):
     name = CharField(max_length=100)
     Book = models.ManyToManyField(Book)
+    
+    def __str__(self):
+        return self.title
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
