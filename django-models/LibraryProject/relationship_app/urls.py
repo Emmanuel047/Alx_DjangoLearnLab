b07f0fd6b2826e4da020a695e7ea7147092e0c8d
Emmanuel047 <emwenwa007@gmail.com>
2025-11-09 23:27:44 +0300

    login

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,15 @@
+from atexit import register
 from django.template import Library
 from django.urls import path
 from django.views.generic.list import ListView
 from . import views
 from .views import list_books, LibraryDetailView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('books/', views.list_books, name='books'),
-    path('library/', LibraryDetailView.as_view(), name='LibraryDetailView')
+    path('library/', LibraryDetailView.as_view(), name='LibraryDetailView'),
+    path('logout', auth_views.LogoutView.as_view(template_name='relationship_app/templates/logout.html'), name='logout'),
+    path('login', auth_views.LoginView.as_view(template_name='relationship_app/templates/login.html'), name='login'),
+    path('register', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.template import library
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -16,3 +17,13 @@ def list_books(request):
 class LibraryDetailView(ListView):
     model = Library
     template_name= 'relationship_app/library_detail.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = user.CreationForm()
+        return render(request, 'relationship_app/templatesregister.html', {'form':form})
