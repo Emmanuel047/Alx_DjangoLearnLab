b92766d42c8609e3cb8518de660421e808d60c6c
Emmanuel047 <emwenwa007@gmail.com>
2025-11-09 17:06:06 +0300

    librarian changes

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,1 +1,8 @@
-["Library.Objects.get(name=library_name)"]
+#List all books in a library.
+Library.Objects.get(name=library_name)
+
+#Query all books by a specific author.
+Book.Objects.filter(author_name=author_name)
+
+#Retrieve the librarian for a library.
+librari = Librarian.Objects.get(name="librarian_name")
