0080952add990f25d3346bcc42722bc62d0e3875
Emmanuel047 <emwenwa007@gmail.com>
2025-11-09 17:10:44 +0300

    slash

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,23 @@
 #List all books in a library.
-Library.Objects.get(name=library_name)
-books_in_library = Library.Book.all()
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.Book.all()
+
+for book in books_in_library:
+    print(book.title)
+
 
 #Query all books by a specific author.
-Book.Objects.filter(author_name=author_name)
+author_name = "J.K. Rowling"
+books_by_author = Book.objects.filter(author__name=author_name)
+
+for book in books_by_author:
+    print(book.title)
+
 
 #Retrieve the librarian for a library.
-librari = Librarian.Objects.get(name="librarian_name")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+librarian = Librarian.objects.get(library=library)
+
+print(librarian.name)
