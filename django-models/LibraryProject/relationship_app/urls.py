19f7ce5302280a51bd1da6d3ac843d9bb585582a
Emmanuel047 <emwenwa007@gmail.com>
2025-11-10 12:28:34 +0300

    auth pages aded

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,7 +3,7 @@ from django.template import Library
 from django.urls import path
 from django.views.generic.list import ListView
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import librarian_view, list_books, LibraryDetailView, member_view
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
@@ -14,4 +14,7 @@ urlpatterns = [
     path('logout', auth_views.LogoutView.as_view(template_name='relationship_app/templates/logout.html'), name='logout'),
     path('login', auth_views.LoginView.as_view(template_name='relationship_app/templates/login.html'), name='login'),
     path('register', views.register, name='register'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view',)
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
+from re import U
 from django.contrib.auth.forms import UserCreationForm
+from django.http import request
 from django.shortcuts import render
 from django.template import library
 from .models import Library
@@ -9,6 +11,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+request
 
 
 # Create your views here.
@@ -30,3 +34,24 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/templates/register.html', {'form':form})
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.profile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render (request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
