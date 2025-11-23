b723d92981dc8149b6645e304538fb667ee37b1d
Emmanuel047 <emwenwa007@gmail.com>
2025-11-23 22:18:59 +0300

    task02

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewset
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r,'book_all', BookViewset, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+
+class BookViewset(viewsets.ModelViewSet):
+    queryset = Book.object.all()
+    serializer_class = BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
