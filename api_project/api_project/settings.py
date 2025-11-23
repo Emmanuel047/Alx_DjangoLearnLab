7588695306942c28e176d783521cc8356b08ab56
Emmanuel047 <emwenwa007@gmail.com>
2025-11-23 22:42:02 +0300

    setttings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'rest_framework.authentication.TokenAuthentication'
+    'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
