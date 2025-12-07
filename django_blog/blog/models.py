2c3387a835ad2fdc328602d69ab616de4e2d650f
Emmanuel047 <emwenwa007@gmail.com>
2025-12-07 20:41:32 +0300

    fixed author typo

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Models):
+    title = models.CharFiield(max_lenght=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey
