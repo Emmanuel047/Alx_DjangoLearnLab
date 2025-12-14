ec86074b18879594142b39dd19e30a7de3c2e4c6
Emmanuel047 <emwenwa007@gmail.com>
2025-12-14 20:58:23 +0300

    accounts.py

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
+
+urlpatterns = [
+    path('register/', views.RegistrationView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
+    path('', include(router.urls)),
+]
