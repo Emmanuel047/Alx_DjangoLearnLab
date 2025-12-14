2029e13dc58005a7e5b0bd24f288fc2410a93895
Emmanuel047 <emwenwa007@gmail.com>
2025-12-14 22:21:00 +0300

    django deployment

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,13 +5,10 @@ from . import views
 router = DefaultRouter()
 router.register(r'posts', views.PostViewSet)
 router.register(r'comments', views.CommentViewSet)
-router.register(r'posts', views.PostViewSet, basename='post')
-router.register(r'comments', views.CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', views.user_feed, name='user_feed'),
     path('posts/<int:pk>/like/', views.like_post, name='like_post'),
     path('posts/<int:pk>/unlike/', views.unlike_post, name='unlike_post'),
-]
 ]
